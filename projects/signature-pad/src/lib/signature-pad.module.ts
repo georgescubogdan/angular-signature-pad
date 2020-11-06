ebfd85d9a19321dc9535eb26cdd73c190c474640
Georgescu Bogdan <georgescubogdan96@gmail.com>
2020-11-06 14:36:08 +0200

    working version angular 10

---
+++ projects/signature-pad/src/lib/signature-pad.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { SignaturePadComponent } from './signature-pad.component';
+
+
+@NgModule({
+  declarations: [SignaturePadComponent],
+  imports: [
+  ],
+  exports: [SignaturePadComponent]
+})
+export class SignaturePadModule { }
---
+++ projects/signature-pad/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of signature-pad
+ */
+
+export * from './lib/signature-pad.service';
+export * from './lib/signature-pad.component';
+export * from './lib/signature-pad.module';
